d8fe85e665cd66c7787dc6ef39cfaffeeed61bbd
Stevensun77 <66322196+Stevensun77@users.noreply.github.com>
2020-07-01 22:11:15 +0800

    Update Year.kt

--- Year.kt
+++ Year.kt
@@ -1,27 +1,25 @@
 package homework
 
 import java.util.*
+import kotlin.random.Random
 
 fun main() {
     var scanner = Scanner(System.`in`)
-//    var number = 0
-    while (true) {
-//    while (number != -1) {
-        print("請輸入年分判斷是否為閏年(輸入-1離開程式): ")
-        var number = scanner.nextInt()
-        if (number == -1) {
-            break
-        } else
-        if (number % 4 == 0 && number % 100 != 0 ) {
-            println("此年為閏年!")
+    var number = 0
+
+    while (number != -1) {
+    print("請輸入年分判斷是否為閏年(輸入-1離開程式): ")
+    var num = scanner.nextInt()
+        if (num % 4 == 0 && num % 100 != 0) {
+            println("西元${num}年為閏年!")
         } else if (number % 400 == 0) {
-            println("此年為閏年!")
+            println("西元${num}年為閏年!")
         } else {
-            println("此年不是閏年!")
+            println("西元${num}年不是閏年!")
         }
-//        else if (number % 4 != 0 || number % 400 != 0) {
-//            println("此年不是閏年!")
-//        }
+        number = num
     }
+
     println("謝謝使用!")
+
 }
